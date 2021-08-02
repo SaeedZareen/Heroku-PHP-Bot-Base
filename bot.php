13388a6b9284f562e1ab137e47f195ebe15c9795
Saeed Zareen <40164278+SaeedZareen@users.noreply.github.com>
2021-08-03 02:33:15 +0430

    Create bot.php

---
+++ bot.php
@@ -0,0 +1,70 @@
+<?php
+
+//دریافت تمامی ورودی ها
+$var = file_get_contents("php://input");
+//تبدیل ورودی ها به آرایه
+$var = json_decode($var,true);
+//دریافت شناسه چت
+$chat_id = $var['message']['chat']['id'];
+//دریافت پیام ارسال شده توسط کاربر
+$text = $var['message']['text'];
+//تعریف توکن ربات
+$token = "*******************";
+
+
+//این تابع یک پیام ساده ارسال میکند
+function sendMessage($chat_id,$text)
+{
+	global $token;
+    $api    = "https://api.telegram.org/bot$token/";
+    $method = "sendMessage";
+    $params = "?chat_id=$chat_id&text=" . urlencode($text);
+  
+  	$url = $api . $method . $params;
+    $result = file_get_contents($url);
+  	return $result;
+}
+//این تابع یک پیام به همراه کیبورد ساده ارسال میکند
+function sendMessageWithKeyboard($chat_id,$text,$reply_markup)
+{
+	global $token;
+    $api    = "https://api.telegram.org/bot$token/";
+    $method = "sendMessage";
+    $params = "?chat_id=$chat_id&text=" . urlencode($text);
+    $params .= "&reply_markup=" . json_encode($reply_markup);
+  
+  	$url = $api . $method . $params;
+    $result = file_get_contents($url);
+  	return $result;
+}
+//تعریف  دکمه های کیبورد
+$keyboard_button = array( ['Button 1','Button 2'] );
+//تعریف کیبورد
+$keyboard = array(
+	'keyboard'			=>	$keyboard_button,
+	'resize_keyboard'	=>	true,
+);
+/*
+اگر پیام دریافتی از کاربر برابر :
+/start
+باشد، این خروجی داده خواهد شد
+*/
+if ( $text == '/start' ) 
+{
+	$message = "Hi There, Welcome...";
+	echo sendMessageWithKeyboard($chat_id,$message,$keyboard);
+}
+//اگر دکمه شماره 1 فشرده شود
+if ( $text == 'Button 1' ) 
+{
+	$message = "Result From Button 1";
+	echo sendMessage($chat_id,$message);
+}
+//اگر دکمه شماره 2 فشرده شود
+if ( $text == 'Button 2' ) 
+{
+	$message = "Result From Button 2";
+	sendMessage($chat_id,$message);
+}
+
+?>
