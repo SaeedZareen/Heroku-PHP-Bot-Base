a009101dc092bdc25220431eefd1a6d2f694f2af
Saeed Zareen <40164278+SaeedZareen@users.noreply.github.com>
2021-08-03 02:32:45 +0430

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+ <title>Congratulations | You did it 😃👏</title>
+</head>
+ <body bgcolor='#FFEBCD'>
+  <center> 
+   <h1>🎉 Congratulations | You did it 😃👏</h1>
+   <p>🔷 To Run Your Robot, Do The Following Stuff 🔶</p>
+   <p>1️⃣ Edit The <code>"bot.php"</code> File in Your Github That Forked From My Repo and Put Your Code in it.</p>
+   <p>2️⃣ Set Your Bot Webhook With This address : "https://api.telegram.org/bot<code>[TOKEN]</code>/setwebhook?url=<code>[DOMAIN]</code>/bot.php"</p>
+   <h4>🔎 Hint » Use Your Token instead of <code>[TOKEN]</code> and Your Site address instead of <code>[DOMAIN]</code></h4>
+   <p>🤖 Enjoy ...</p>
+   <p>🌐 if You Have any Question You Can Contact Me in Telegram : <a href="tg://resolve?domain=EMlNEM">Saeed Zareen</a></p>
+  </center>
+ </body>
+</html>
